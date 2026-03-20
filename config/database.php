2024d954187722c2e97e82cadd5ceb808d47cd09
I Made Brahmanda Setyadi <brahmandasetyadi00@gmail.com>
2026-03-21 01:42:29 +0800

    feat: updae new folder

--- config/database.php
+++ config/database.php
@@ -5,15 +5,15 @@
 $host = 'localhost';    // Biasanya 'localhost'
 $db_name = 'db_jpt_grosir'; // Nama database yang sudah Anda buat
 $username = 'root';         // Username default XAMPP
-$password = '';             // Password default XAMPP (kosong)
+$password = 'root';             // Password default XAMPP (kosong)
 
 try {
     // Membuat koneksi menggunakan PDO (PHP Data Objects)
     $pdo = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-    
+
     // Mengatur mode error PDO ke exception untuk penanganan error yang lebih baik
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+
 } catch (PDOException $e) {
     // Jika koneksi gagal, tampilkan pesan error dan hentikan skrip
     die("Koneksi ke database gagal: " . $e->getMessage());
